8179ebae6858cb5da8e0f315c80fe9185f2d3133
Lucravisk <165109896+Lucravisk@users.noreply.github.com>
2024-03-28 14:04:55 -0300

    Add files via upload

---
+++ Desafio 1.py
@@ -0,0 +1,9 @@
+nome=input('qual seu nome?')
+print('Olá', nome,'! prazer em conhece-lo')
+print('Quando você nasceu?')
+
+dia=input('dia =')
+mes=input('mes =')
+ano=input('ano =')
+print('você nasceu no dia',dia,'de',mes,'de',ano,',correto?')
+
